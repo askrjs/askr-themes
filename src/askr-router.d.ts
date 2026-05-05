d6d707862c71e6722b2479c529aadf31ff4e4da9
smiggleworth <jrepanich@gmail.com>
2026-05-05 13:26:08 -0400

    chore: update package version to 0.0.2 and adjust publish workflow name

--- src/askr-router.d.ts
+++ src/askr-router.d.ts
@@ -1,18 +1,14 @@
 declare module "@askrjs/askr/router" {
-  export type LinkProps = Omit<JSX.IntrinsicElements["a"], "children" | "href" | "class" | "rel" | "target" | "aria-current" | "aria-label"> & {
+  export type LinkProps = Omit<
+    JSX.IntrinsicElements["a"],
+    "children" | "href" | "class" | "rel" | "target" | "aria-current" | "aria-label"
+  > & {
     href: string;
     class?: string;
     children?: unknown;
     rel?: string;
     target?: string;
-    "aria-current"?:
-      | "page"
-      | "step"
-      | "location"
-      | "date"
-      | "time"
-      | "true"
-      | "false";
+    "aria-current"?: "page" | "step" | "location" | "date" | "time" | "true" | "false";
     "aria-label"?: string;
   };
 
